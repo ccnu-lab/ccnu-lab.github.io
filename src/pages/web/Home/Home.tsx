91323a16ab6c4c4b0a6570d77a48f928b831a54e
tninenine <cczhang319@qq.com>
2023-02-15 13:43:35 +0800

    test

--- src/pages/web/Home/Home.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Home() {
-  return <a>主页</a>;
-}
--- src/pages/web/Introduction/Introduction.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Introduction() {
-  return <a>项目简介</a>;
-}
