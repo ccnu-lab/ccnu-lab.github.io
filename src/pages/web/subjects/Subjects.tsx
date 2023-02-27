e13cc9389610e9424e733afadba951c51ad8459d
lx <2444171739@qq.com>
2023-02-27 15:19:18 +0800

    task:[解决课程设置页面内容无法居中问题#25]

--- src/pages/web/subjects/Subjects.tsx
+++ src/pages/web/subjects/Subjects.tsx
@@ -3,7 +3,7 @@ import {Card, Typography} from 'antd';
 import styled from 'styled-components';
 import tab_bg from '@/assets/images/tab_bg.jpg';
 import subjects from '@/constants/subjects';
-import {FlexWrap, TitleWrap, UnderLineWrap,PrincipalBox} from '@/style/styles';
+import {FlexWrap, TitleWrap, UnderLineWrap, PrincipalBox} from '@/style/styles';
 
 const {Title} = Typography;
 
@@ -14,7 +14,7 @@ const FlexColumnWrap = styled(FlexWrap)`
 const FlexLeftWrap = styled.div`
   display: flex;
   flex-wrap: wrap;
-`
+`;
 
 const PrincipalBoxWarp = styled(PrincipalBox)`
   margin: 60px 29px 85px 0;
@@ -46,37 +46,37 @@ const CardWarp = styled(Card)`
   box-shadow: 0px 8px 12px 1px rgba(102, 120, 166, 0.03), 0px 5px 9px -3px rgba(102, 120, 166, 0.05);
 `;
 
-
-
 export default function Subjects() {
   return (
-    <FlexColumnWrap>
+    <FlexWrap>
       <img src={tab_bg} />
-      {subjects.map((subject, index) => {
-        return (
-          <CardWarp key={index}>
-            <TitleWrap style={{justifyContent: 'left'}}>
-              <Title level={1} style={{margin: '0', padding: '0 0 20px 0'}}>
-                {subject.title}
-              </Title>
-            </TitleWrap>
-            <div>
-              <UnderLineWrap style={{width: '405px'}} />
-            </div>
-            <FlexLeftWrap>
-              <PrincipalBoxWarp>
-                <img src={subject.imgsrc} alt="" />
-                <div className="principalName">{subject.leader}</div>
-                <div className="introduction">项目负责人</div>
-              </PrincipalBoxWarp>
-              <SubjectInfo>
-                <div className="title">研究内容</div>
-                <div className="context">{subject.context}</div>
-              </SubjectInfo>
-            </FlexLeftWrap>
-          </CardWarp>
-        );
-      })}
-    </FlexColumnWrap>
+      <FlexColumnWrap>
+        {subjects.map((subject, index) => {
+          return (
+            <CardWarp key={index}>
+              <TitleWrap style={{justifyContent: 'left'}}>
+                <Title level={1} style={{margin: '0', padding: '0 0 20px 0'}}>
+                  {subject.title}
+                </Title>
+              </TitleWrap>
+              <div>
+                <UnderLineWrap style={{width: '405px'}} />
+              </div>
+              <FlexLeftWrap>
+                <PrincipalBoxWarp>
+                  <img src={subject.imgsrc} alt="" />
+                  <div className="principalName">{subject.leader}</div>
+                  <div className="introduction">项目负责人</div>
+                </PrincipalBoxWarp>
+                <SubjectInfo>
+                  <div className="title">研究内容</div>
+                  <div className="context">{subject.context}</div>
+                </SubjectInfo>
+              </FlexLeftWrap>
+            </CardWarp>
+          );
+        })}
+      </FlexColumnWrap>
+    </FlexWrap>
   );
 }
