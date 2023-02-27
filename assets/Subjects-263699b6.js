760bdc64ba0091e81eb5e7fdd9d6c7109781311f
tninenine <cczhang319@qq.com>
2023-02-27 13:51:55 +0800

    Updates

---
+++ assets/Subjects-263699b6.js
@@ -0,0 +1,32 @@
+import{j as t}from"./main-4e1cd931.js";import{s as e}from"./styled-components.browser.esm-3a1d7e2e.js";import{t as x}from"./tab_bg-523399b6.js";import{p as i}from"./principal_default-d1aea06c.js";import{T as n,U as l,F as a,P as p}from"./styles-794da372.js";import{T as o,f as c}from"./antd-59c365fd.js";import"./react-f0d12c14.js";const d=[{title:"课题1:连续学习的理论与模型",imgsrc:i,leader:"张三",context:"从理论模型研究入手，聚焦多源异构数据的多重知识表达体系构建（研究内容1）、抗灾难性遗忘的稳健知识增扩（研究内容2）和人在回路的模型能力成长方法（研究内容3），旨在通过建立知识和数据相融合的连续学习理论模型，实现连续学习过程中的抗灾难性遗忘知识表达和稳健知识增扩，进而实现智能系统的主动模式探知和自主连续学习"},{title:"课题1:连续学习的理论与模型",imgsrc:i,leader:"张三",context:"从理论模型研究入手，聚焦多源异构数据的多重知识表达体系构建（研究内容1）、抗灾难性遗忘的稳健知识增扩（研究内容2）和人在回路的模型能力成长方法（研究内容3），旨在通过建立知识和数据相融合的连续学习理论模型，实现连续学习过程中的抗灾难性遗忘知识表达和稳健知识增扩，进而实现智能系统的主动模式探知和自主连续学习"},{title:"课题1:连续学习的理论与模型",imgsrc:i,leader:"张三",context:"从理论模型研究入手，聚焦多源异构数据的多重知识表达体系构建（研究内容1）、抗灾难性遗忘的稳健知识增扩（研究内容2）和人在回路的模型能力成长方法（研究内容3），旨在通过建立知识和数据相融合的连续学习理论模型，实现连续学习过程中的抗灾难性遗忘知识表达和稳健知识增扩，进而实现智能系统的主动模式探知和自主连续学习"},{title:"课题1:连续学习的理论与模型",imgsrc:i,leader:"张三",context:"从理论模型研究入手，聚焦多源异构数据的多重知识表达体系构建（研究内容1）、抗灾难性遗忘的稳健知识增扩（研究内容2）和人在回路的模型能力成长方法（研究内容3），旨在通过建立知识和数据相融合的连续学习理论模型，实现连续学习过程中的抗灾难性遗忘知识表达和稳健知识增扩，进而实现智能系统的主动模式探知和自主连续学习"},{title:"课题1:连续学习的理论与模型",imgsrc:i,leader:"张三",context:"从理论模型研究入手，聚焦多源异构数据的多重知识表达体系构建（研究内容1）、抗灾难性遗忘的稳健知识增扩（研究内容2）和人在回路的模型能力成长方法（研究内容3），旨在通过建立知识和数据相融合的连续学习理论模型，实现连续学习过程中的抗灾难性遗忘知识表达和稳健知识增扩，进而实现智能系统的主动模式探知和自主连续学习"}],m=d,{Title:g}=o,f=e(a)`
+  flex-direction: column;
+`,h=e.div`
+  display: flex;
+  flex-wrap: wrap;
+`,j=e(p)`
+  margin: 60px 29px 85px 0;
+`,u=e.div`
+  display: flex;
+  flex-direction: column;
+  width: 769px;
+  height: 219px;
+  margin: 60px 0 119px 0;
+  .title {
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 30px;
+    text-align: justify;
+    margin-bottom: 10px;
+  }
+  .context {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 30px;
+    text-align: justify;
+  }
+`,y=e(c)`
+  width: 1200px;
+  margin: -103px 0 143px 0;
+  padding: 57px 0 0 60px;
+  box-shadow: 0px 8px 12px 1px rgba(102, 120, 166, 0.03), 0px 5px 9px -3px rgba(102, 120, 166, 0.05);
+`;function F(){return t.jsxs(f,{children:[t.jsx("img",{src:x}),m.map((r,s)=>t.jsxs(y,{children:[t.jsx(n,{style:{justifyContent:"left"},children:t.jsx(g,{level:1,style:{margin:"0",padding:"0 0 20px 0"},children:r.title})}),t.jsx("div",{children:t.jsx(l,{style:{width:"405px"}})}),t.jsxs(h,{children:[t.jsxs(j,{children:[t.jsx("img",{src:r.imgsrc,alt:""}),t.jsx("div",{className:"principalName",children:r.leader}),t.jsx("div",{className:"introduction",children:"项目负责人"})]}),t.jsxs(u,{children:[t.jsx("div",{className:"title",children:"研究内容"}),t.jsx("div",{className:"context",children:r.context})]})]})]},s))]})}export{F as default};
--- assets/styles-794da372.js
+++ assets/styles-794da372.js
@@ -1,4 +1,4 @@
-import{s as t}from"./styled-components.browser.esm-3a1d7e2e.js";import{f as e}from"./antd-59c365fd.js";const p=t.div`
+import{s as t}from"./styled-components.browser.esm-3a1d7e2e.js";import{f as e}from"./antd-59c365fd.js";const o=t.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -24,7 +24,6 @@ import{s as t}from"./styled-components.browser.esm-3a1d7e2e.js";import{f as e}fr
     margin: 0;
   }
   .ant-typography {
-    font-style: normal;
     font-weight: 800px;
     font-size: 32px;
     line-height: 39px;
@@ -41,11 +40,11 @@ import{s as t}from"./styled-components.browser.esm-3a1d7e2e.js";import{f as e}fr
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
-`,s=t.div`
+`,d=t.div`
   height: 1px;
   background-color: ${i=>i.lineColor?i.lineColor:" #000000"};
   width: ${i=>i.lineWidth?i.lineWidth:"100%"};
-`,d=t.div`
+`,s=t.div`
   font-weight: 400;
   font-size: 16px;
   line-height: 30px;
@@ -87,4 +86,4 @@ import{s as t}from"./styled-components.browser.esm-3a1d7e2e.js";import{f as e}fr
     font-size: 12px;
     line-height: 20px;
   }
-`;export{d as C,x as F,p as L,f as P,r as T,s as U,a};
+`;export{s as C,x as F,o as L,f as P,r as T,d as U,a};
