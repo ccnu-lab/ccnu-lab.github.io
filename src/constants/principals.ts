9bccc53c631a82f9efdd4d16a33452608fc296bd
tninenine <cczhang319@qq.com>
2023-03-01 14:01:08 +0800

    task:[CCNU-LAB-33]

--- src/constants/principals.ts
+++ src/constants/principals.ts
@@ -1,9 +1,9 @@
 import principal_default from '@/assets/images/principal_default.png';
 const principals = [
-  {image: principal_default, name: '张三'},
-  {image: principal_default, name: '张三'},
-  {image: principal_default, name: '张三'},
-  {image: principal_default, name: '张三'},
-  {image: principal_default, name: '张三'},
+  {id: 1, image: principal_default, name: '张三'},
+  {id: 2, image: principal_default, name: '张三'},
+  {id: 3, image: principal_default, name: '张三'},
+  {id: 4, image: principal_default, name: '张三'},
+  {id: 5, image: principal_default, name: '张三'},
 ];
 export default principals;
