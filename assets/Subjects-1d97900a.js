cc0079c1a7589a88e81fc4030a1323cf16045061
tninenine <cczhang319@qq.com>
2023-03-01 14:02:42 +0800

    Updates

---
+++ assets/Subjects-1d97900a.js
@@ -0,0 +1,32 @@
+import{j as t,F as r,t as n,T as l,U as a,s as i,P as p}from"./main-495018ab.js";import{p as e}from"./principal_default-d1aea06c.js";import{T as c,C as o}from"./antd-6d5fccc9.js";import"./react-fcd6345f.js";const d=[{title:"课题1:连续学习的理论与模型",imgsrc:e,leader:"张三",context:"从理论模型研究入手，聚焦多源异构数据的多重知识表达体系构建（研究内容1）、抗灾难性遗忘的稳健知识增扩（研究内容2）和人在回路的模型能力成长方法（研究内容3），旨在通过建立知识和数据相融合的连续学习理论模型，实现连续学习过程中的抗灾难性遗忘知识表达和稳健知识增扩，进而实现智能系统的主动模式探知和自主连续学习"},{title:"课题1:连续学习的理论与模型",imgsrc:e,leader:"张三",context:"从理论模型研究入手，聚焦多源异构数据的多重知识表达体系构建（研究内容1）、抗灾难性遗忘的稳健知识增扩（研究内容2）和人在回路的模型能力成长方法（研究内容3），旨在通过建立知识和数据相融合的连续学习理论模型，实现连续学习过程中的抗灾难性遗忘知识表达和稳健知识增扩，进而实现智能系统的主动模式探知和自主连续学习"},{title:"课题1:连续学习的理论与模型",imgsrc:e,leader:"张三",context:"从理论模型研究入手，聚焦多源异构数据的多重知识表达体系构建（研究内容1）、抗灾难性遗忘的稳健知识增扩（研究内容2）和人在回路的模型能力成长方法（研究内容3），旨在通过建立知识和数据相融合的连续学习理论模型，实现连续学习过程中的抗灾难性遗忘知识表达和稳健知识增扩，进而实现智能系统的主动模式探知和自主连续学习"},{title:"课题1:连续学习的理论与模型",imgsrc:e,leader:"张三",context:"从理论模型研究入手，聚焦多源异构数据的多重知识表达体系构建（研究内容1）、抗灾难性遗忘的稳健知识增扩（研究内容2）和人在回路的模型能力成长方法（研究内容3），旨在通过建立知识和数据相融合的连续学习理论模型，实现连续学习过程中的抗灾难性遗忘知识表达和稳健知识增扩，进而实现智能系统的主动模式探知和自主连续学习"},{title:"课题1:连续学习的理论与模型",imgsrc:e,leader:"张三",context:"从理论模型研究入手，聚焦多源异构数据的多重知识表达体系构建（研究内容1）、抗灾难性遗忘的稳健知识增扩（研究内容2）和人在回路的模型能力成长方法（研究内容3），旨在通过建立知识和数据相融合的连续学习理论模型，实现连续学习过程中的抗灾难性遗忘知识表达和稳健知识增扩，进而实现智能系统的主动模式探知和自主连续学习"}],m=d,{Title:g}=c,h=i(r)`
+  flex-direction: column;
+`,j=i.div`
+  display: flex;
+  flex-wrap: wrap;
+`,f=i(p)`
+  margin: 60px 29px 85px 0;
+`,u=i.div`
+  display: flex;
+  flex-direction: column;
+  width: 769px;
+  height: 219px;
+  margin: 60px 0 119px 0;
+  .title {
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 30px;
+    text-align: justify;
+    margin-bottom: 10px;
+  }
+  .context {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 30px;
+    text-align: justify;
+  }
+`,y=i(o)`
+  width: 1200px;
+  margin: -103px 0 143px 0;
+  padding: 57px 0 0 60px;
+  box-shadow: 0px 8px 12px 1px rgba(102, 120, 166, 0.03), 0px 5px 9px -3px rgba(102, 120, 166, 0.05);
+`;function C(){return t.jsxs(r,{children:[t.jsx("img",{src:n}),t.jsx(h,{children:m.map((s,x)=>t.jsxs(y,{children:[t.jsx(l,{style:{justifyContent:"left"},children:t.jsx(g,{level:1,style:{margin:"0",padding:"0 0 20px 0"},children:s.title})}),t.jsx("div",{children:t.jsx(a,{style:{width:"405px"}})}),t.jsxs(j,{children:[t.jsxs(f,{children:[t.jsx("img",{src:s.imgsrc,alt:""}),t.jsx("div",{className:"principalName",children:s.leader}),t.jsx("div",{className:"introduction",children:"项目负责人"})]}),t.jsxs(u,{children:[t.jsx("div",{className:"title",children:"研究内容"}),t.jsx("div",{className:"context",children:s.context})]})]})]},x))})]})}export{C as default};
